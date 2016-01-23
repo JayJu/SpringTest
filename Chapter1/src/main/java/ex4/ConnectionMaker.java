f1cda1268031d4b93ca747fcf5f723cdfc3806e6
jin <iam@juheejin.com>
2016-01-23 13:47:10 +0900

    sprint test

--- Chapter1/src/main/java/ex4/ConnectionMaker.java
+++ Chapter1/src/main/java/ex4/ConnectionMaker.java
@@ -7,5 +7,5 @@ import java.sql.SQLException;
  * Created by 1015331 on 2016-01-16.
  */
 public interface ConnectionMaker {
-    public Connection makeConnection() throws ClassNotFoundException, SQLException;
+    Connection makeConnection() throws ClassNotFoundException, SQLException;
 }

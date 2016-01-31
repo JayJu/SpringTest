1fec11d0c3e38d079806735f244aef4f3d304236
jin <iam@juheejin.com>
2016-01-31 09:39:15 +0900

    sprint test

---
+++ Chapter3/src/main/java/ex5/p2/StatementStrategy.java
@@ -0,0 +1,12 @@
+package ex5.p2;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * Created by jin on 2016. 1. 27..
+ */
+public interface StatementStrategy {
+    PreparedStatement makePreparedStatement(Connection c) throws SQLException;
+}
